91325a8ec419401bb09dae777d6b970758d1cc85
Isaac Juarez <isaacjuarezij1@gmail.com>
2025-03-03 15:17:28 -0800

    Functions Done!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var lastMessageNumber = -1 //lastMessage will never be -1
+    @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10 // images labeled image0 - image 9
+    let numberOfSounds = 6 //sounds labeled sound0 - sound
+    
     var body: some View {
-        
+    
         VStack {
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
             Spacer()
             
             Image(imageName)
@@ -21,27 +38,56 @@ struct ContentView: View {
                 .foregroundStyle(.orange)
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
-            Button("Press Me!") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
-             
-                
-                message = (message == message1 ? message2 : message1)
-                imageName = ( imageName == "image0" ? "image1" : "image0")
             
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
+                                "You Are Great",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!"]
+               
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                message = messages [lastMessageNumber]
+                
+                
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastImageNumber)"
+
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                playSound(soundName:  "sound\(lastSoundNumber)")
         }
         .buttonStyle(.borderedProminent)
         .font(.title2)
     }
         .padding()
 }
+    
+    func nonRepeatingRandom(lastNumber: Int, upperBound: Int) -> Int {
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperBound)
+        }   while newNumber == lastNumber
+        return newNumber
+
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print ("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription)creating audioPlayer ")
+            
+        }
+    }
 }
 
 #Preview {
